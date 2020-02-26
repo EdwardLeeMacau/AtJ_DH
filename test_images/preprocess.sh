fd920cf664232769c49ba798771411ab438e78f4
EdwardLeeMacau <b05901119@ntu.edu.tw>
2020-02-26 17:08:54 +0800

    Add Image Preprocess Script

---
+++ test_images/preprocess.sh
@@ -0,0 +1,2 @@
+python3.6 resize_crop_shift.py --unprocessed_dir ./I_space --processed_dir ./I_space_patches
+python3.6 resize_crop_shift.py --unprocessed_dir ./J_original --processed_dir ./J_ori_patches
