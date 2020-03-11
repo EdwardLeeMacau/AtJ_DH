ad5686a3ca4eaaa4d3e65b4f6f280345d28ae4df
EdwardLeeMacau <b05901119@ntu.edu.tw>
2020-03-11 19:40:50 +0800

    Modify script

--- script/test.sh
+++ script/test.sh
@@ -4,12 +4,6 @@ echo python3 MAINTHREAD_test.py \
     && python3 MAINTHREAD_test.py \
         --cuda \
         --model ./pretrained_model/AtJ_DH_MaxCKPT.pth \
-        --test ./test_images/NTIRE2020_RAW/val/Hazy \
-        --gt ./test_image/NTIRE2020_RAW/val/GT \
-        --outdir ./test_images/NTIRE2020_RAW/val/DeHazy \
-        --rehaze ./test_images/NTIRE2020_RAW/val/ReHazy
+        --test ./test_images/NTIRE2020_RAW/test/Hazy \
+        --outdir ./test_images/NTIRE2020_RAW/test/DeHazy
 
-echo python3 metric.py \
-    && python3 metric.py \
-        --GT_dir ./test_images/NTIRE2020_RAW/val/GT \
-        --DH_dir ./test_images/NTIRE2020_RAW/val/DeHazy
---
+++ submission/readme.txt
@@ -0,0 +1,4 @@
+runtime per image [s] :  
+CPU[1] / GPU[0] : 0
+Extra Data [1] / No Extra Data [0] : 0
+Other description : 
